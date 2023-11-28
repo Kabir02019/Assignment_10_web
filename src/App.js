6c6bd08fbf44020e0ad7780cc806a78b3199cd41
Kabir Khanna <kabirkhanna@Kabirs-MacBook-Pro.local>
2023-11-28 16:37:55 -0500

    Submission

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from'react';
+import DayList from './Components/DayList'
+import HourlyData from './Components/HourlyData'
+import { useState } from 'react';
+import {useEffect} from "react";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import NotFound from './Components/NotFound'
+
+
+function App(){
+       
+    const [data,setData]=useState([]);
+    const [showData,setShowData]=useState(false);
+
+
+    const fetchData = async () => {
+        try {
+          const response = await fetch(
+            'https://api.openweathermap.org/data/2.5/forecast?lat=42.361145&lon=-71.057083&appid=b30b3d53cba100e58a4ade227ca2e9de&units=metric'
+          );
+          const result = await response.json();
+          const array=result.list;
+          console.log(array);
+
+          setData(array);
+
+
+
+        } catch (err) {
+          console.error(err);
+        } finally {
+            setShowData(true);
+        }
+      };
+    
+      useEffect(() => {
+        fetchData();
+        
+      }, []); // Empty dependency array means this effect runs once after the initial render
+    
+    
+    
+return(<>
+
+
+    <div className='container'>
+
+    <h1>Weather Forecast</h1>
+   
+    <BrowserRouter>
+<Routes>
+    <Route path="/" element={showData? <DayList records={data}/> : 'No data found'}> 
+    <Route path="details" element={<HourlyData />}/> 
+    <Route path="*" element={<NotFound/>}/>
+    </Route>
+</Routes>
+</BrowserRouter>
+    </div>
+    </>
+)
+
+}
+
+export default App;
+
